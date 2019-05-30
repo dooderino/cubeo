c25b430e376127d33cdd030b8bc7888bcc756836
dooderino <jspataro@gmail.com>
2019-05-29 17:05:50 -0700

    content

--- src/Game.hx
+++ src/Game.hx
@@ -1,32 +1,18 @@
+import hxd.Res;
 import hxd.res.DefaultFont;
-import h2d.Graphics;
 import h2d.Flow;
 import h2d.Text;
-import h3d.scene.*;
 
 class Game extends hxd.App {
 	public var fps : Text;
 	public var fps_flow : Flow;
 	public var flow : Flow;
-	public var world : World;
-	public var graphics : h2d.Graphics;
 
 	override function new() {
 		super();
 	}
 
 	override function init() {
-		graphics = new h2d.Graphics(s2d);
-		world = new h3d.scene.World(64, 128, s3d);
-
-		s3d.camera.pos.set(0, 200, 0);
-	
-		s3d.lightSystem.ambientLight.set(0.9, 0.9, 0.9);
-
-		var shadow = s3d.renderer.getPass(h3d.pass.DefaultShadowMap);
-		shadow.power = 1;
-		shadow.color.setColor(0x301030);
-
 	    flow = new h2d.Flow(s2d);
 	    flow.padding = 20;
 
