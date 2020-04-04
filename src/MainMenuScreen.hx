777df14e0592d6db52d14d07d54093887ee4e576
dooderino <jspataro@gmail.com>
2020-04-04 10:31:27 -0700

    Adding button stuff

--- src/MainMenuScreen.hx
+++ src/MainMenuScreen.hx
@@ -1,12 +1,19 @@
 package;
 
-import h2d.Object;
-import h2d.TextInput;
-import h2d.Font;
 import Game;
 import GameStates;
+import h2d.Interactive;
+import h2d.Font;
+import h2d.Graphics;
+import h2d.Object;
+import h2d.Text;
+import h2d.TextInput;
 
 class MainMenuScreen extends Screen {
+    var hostButton : Graphics;
+    var hostText : Text;
+    var joinButton : Graphics;
+    var joinText : Graphics;
     var ipAddress : TextInput;
     var port : TextInput;
     var font : Font;    
@@ -21,6 +28,49 @@ class MainMenuScreen extends Screen {
         Game.inst.gameState = new GameState();
 
         font = hxd.res.DefaultFont.get();
+
+        hostButton = new h2d.Graphics(object);
+        var RW = 100;
+        var RH = 30;
+        var size = RW - RH;
+        var k = 10;
+
+        hostButton.beginFill(0xFFFFFFFF);
+        for( i in 0...k+1 ) {
+            var a = Math.PI * i / k - Math.PI / 2;
+            hostButton.lineTo(size + RH * Math.cos(a), RH * Math.sin(a));
+        }
+        for( i in 0...k+1 ) {
+            var a = Math.PI * i / k + Math.PI / 2;
+            hostButton.lineTo(-size + RH * Math.cos(a), RH * Math.sin(a));
+        }
+        hostButton.endFill();
+
+        hostButton.x = Game.inst.sceneWidth/2 + 200;
+        hostButton.y = Game.inst.sceneHeight/2 - 60;
+
+        hostText = new Text(font, hostButton);
+        hostText.text = "Host Game";
+        hostText.textColor = 0x000000;
+        hostText.textAlign = Align.Center;
+        hostText.y -= 20;
+        hostText.scale(2.5);
+
+        joinButton = new h2d.Graphics(object);
+        joinButton.beginFill(0xFFFFFFFF);
+        for( i in 0...k+1 ) {
+            var a = Math.PI * i / k - Math.PI / 2;
+            joinButton.lineTo(size + RH * Math.cos(a), RH * Math.sin(a));
+        }
+        for( i in 0...k+1 ) {
+            var a = Math.PI * i / k + Math.PI / 2;
+            joinButton.lineTo(-size + RH * Math.cos(a), RH * Math.sin(a));
+        }
+        joinButton.endFill();
+
+        joinButton.x = Game.inst.sceneWidth/2 + 200;
+        joinButton.y = Game.inst.sceneHeight/2 + 60;
+
         
         ipAddress = new TextInput(font, object);
         ipAddress.scale(2);
@@ -70,6 +120,7 @@ class MainMenuScreen extends Screen {
     public override function cleanup() {
         super.cleanup();
         font.dispose();
+        parent.removeChild(joinButton);
         parent.removeChild(ipAddress);
         parent.removeChild(port);
     }
